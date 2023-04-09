213cfc72209d33559afce12913012354cb9fb4f9
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-10 00:43:45 +0300

    Create sign in

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -33,6 +33,30 @@ const createUserController = expressHandler(async (req, res) => {
   }
 });
 
+const signin = expressHandler(async (req, res) => {
+  try {
+    const { email } = req?.body;
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: "Invalid email" });
+    }
+
+    const token = makeToken(user);
+    await sendEmail(
+      "Sign in to StromaWebApp",
+      `<h2>Hello There</h2>
+      <a href=http://localhost:9000/api/user?token=${token}>WebApp Link</a>`,
+      email,
+      res
+    );
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+});
+
 module.exports = {
   createUserController,
+  signin,
 };
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { createUserController } = require("../controllers/userControllers");
+const {
+  createUserController,
+  signin,
+} = require("../controllers/userControllers");
 
 const userRoutes = express.Router();
 
 userRoutes.post("/create", createUserController);
+userRoutes.post("/signin", signin);
 
 module.exports = userRoutes;
