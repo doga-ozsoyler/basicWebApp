b9397c088f4997fdf828677e43cc4d70527d3f55
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-09 19:55:59 +0300

    Connect db

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const dbConnect = require("./config/dbConnect");
 require("dotenv").config();
 const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
+
+dbConnect();
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
