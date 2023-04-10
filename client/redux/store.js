300782654ca7fd5641dac82bf141a875b09f429a
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-11 01:41:30 +0300

    Fix serializable error

--- client/redux/store.js
+++ client/redux/store.js
@@ -3,6 +3,10 @@ import userReducer from "./slices/userReducer";
 
 const store = configureStore({
   reducer: { user: userReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
