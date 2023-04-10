3a006cb41beea445b3368df1275f08a9890b738d
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-11 02:15:38 +0300

    Add modal to inform user

--- client/screens/Signin.js
+++ client/screens/Signin.js
@@ -2,13 +2,20 @@ import React, { useState } from "react";
 import { Input, Center, Button } from "native-base";
 import { signinAction } from "../redux/slices/userReducer";
 import { useDispatch } from "react-redux";
+import InfoModal from "../components/InfoModal";
 
 const SigninScreen = () => {
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <Center flex={1}>
+      <InfoModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        infoText="Click the link in email to sign in"
+      />
       <Input
         value={email}
         onChangeText={setEmail}
@@ -18,6 +25,7 @@ const SigninScreen = () => {
       <Button
         onPress={() => {
           dispatch(signinAction(email));
+          setShowModal(true);
         }}
         margin={5}
         size="sm"
