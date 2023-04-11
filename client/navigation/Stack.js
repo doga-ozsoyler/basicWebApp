32409a60a7481a8fe22b87611a439b6bf8f55087
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-11 15:45:41 +0300

    Add home page

--- client/navigation/Stack.js
+++ client/navigation/Stack.js
@@ -1,18 +1,30 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import SigninScreen from "../screens/Signin";
 import EnterCodeScreen from "../screens/EnterCode";
+import HomeScreen from "../screens/Home";
+import { useSelector } from "react-redux";
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
+  const token = useSelector((state) => {
+    return state?.user?.token;
+  });
+
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="Signin" component={SigninScreen} />
-      <Stack.Screen name="EnterCode" component={EnterCodeScreen} />
+      {token ? (
+        <Stack.Screen name="Home" component={HomeScreen} />
+      ) : (
+        <>
+          <Stack.Screen name="Signin" component={SigninScreen} />
+          <Stack.Screen name="EnterCode" component={EnterCodeScreen} />
+        </>
+      )}
     </Stack.Navigator>
   );
 };
---
+++ client/screens/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Center, Text } from "native-base";
+
+const HomeScreen = () => {
+  return (
+    <Center flex={1}>
+      <Text bold>Wellcome</Text>
+    </Center>
+  );
+};
+
+export default HomeScreen;
