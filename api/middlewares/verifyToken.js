85459a463f9a5ef517d41519382cce7062f75770
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-11 17:25:02 +0300

    Add middleware

---
+++ api/middlewares/verifyToken.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user";
+require("dotenv").config();
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    const token = req?.headers?.authorization;
+    if (!token)
+      return res.json({ success: false, message: "Invalid Authentication" });
+
+    const decoded = jwt.verify(token, `${process.env.JWT_SECRET_KEY}`);
+    if (!decoded)
+      return res.json({ success: false, message: "Invalid Authentication" });
+
+    const user = await User.findById({ _id: decoded.id }).select("-enterCode");
+    if (!user)
+      return res.json({ success: false, message: "User doesn't exist" });
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.json({ success: false, error });
+  }
+};
