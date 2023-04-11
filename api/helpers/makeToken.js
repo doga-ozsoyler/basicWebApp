59bedc199cc5eb0e680c93bea815bd8f1656619c
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-11 17:27:30 +0300

    Update token to get just user id

--- api/helpers/makeToken.js
+++ api/helpers/makeToken.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const makeToken = (user) => {
+const makeToken = (userID) => {
   const expirationDate = new Date();
   expirationDate.setHours(new Date().getHours() + 1);
-  return jwt.sign({ user }, process.env.JWT_SECRET_KEY, {
+  return jwt.sign({ _id: userID }, process.env.JWT_SECRET_KEY, {
     expiresIn: "1d",
   });
 };
