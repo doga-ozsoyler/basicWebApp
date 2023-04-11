e5e58671c370468ea478d102ab8982f10d575790
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-11 17:01:38 +0300

    Use Logout

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { Center, Text } from "native-base";
+import { Button, Center, Text } from "native-base";
+import { useDispatch } from "react-redux";
+import { logoutAction } from "../redux/slices/userReducer";
+import { useNavigation } from "@react-navigation/core";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
     <Center flex={1}>
-      <Text bold>Wellcome</Text>
+      <Text fontSize="3xl" bold>
+        Welcome
+      </Text>
+      <Button marginTop={5}>Invite User</Button>
+      <Button
+        variant="link"
+        onPress={() => {
+          dispatch(logoutAction());
+        }}
+      >
+        Logout
+      </Button>
     </Center>
   );
 };
