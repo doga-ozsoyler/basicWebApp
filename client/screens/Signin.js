a752ac09bb2e763a2549aa88e65211c895c274fc
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-11 11:17:45 +0300

    Add email validation

--- client/screens/Signin.js
+++ client/screens/Signin.js
@@ -3,19 +3,36 @@ import { Input, Center, Button } from "native-base";
 import { signinAction } from "../redux/slices/userReducer";
 import { useNavigation } from "@react-navigation/native";
 import { useDispatch } from "react-redux";
+import FormController from "../components/FormController";
 
 const SigninScreen = () => {
   const dispatch = useDispatch();
   const navigation = useNavigation();
   const [email, setEmail] = useState("");
+  const [emailValidation, setEmailValidation] = useState(true);
+
+  const validateEmail = (text) => {
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+    if (reg.test(text) === false) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const handleEmailText = (text) => {
+    setEmail(text);
+    setEmailValidation(validateEmail(text));
+  };
 
   return (
     <Center flex={1}>
-      <Input
+      <FormController
+        label="Enter Email"
+        message={"Email is Not Correct!"}
+        errorMessageShow={!emailValidation}
         value={email}
-        onChangeText={setEmail}
-        placeholder="Email"
-        w="300"
+        onChangeText={handleEmailText}
       />
       <Button
         onPress={() => {
