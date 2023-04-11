2bf00650472dd198c56e101b47d41d1a387ebdcf
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-11 13:23:01 +0300

    Add check code controller

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   createUserController,
   signin,
+  checkEnterCodeController,
 } = require("../controllers/userControllers");
 
 const userRoutes = express.Router();
 
 userRoutes.post("/create", createUserController);
 userRoutes.post("/signin", signin);
+userRoutes.post("/checkEnterCode", checkEnterCodeController);
 
 module.exports = userRoutes;
