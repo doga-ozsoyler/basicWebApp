8b667e61b30967beac41deb04385154dfcd8b200
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-11 14:03:41 +0300

    Use check enter code action

--- client/screens/EnterCode.js
+++ client/screens/EnterCode.js
@@ -1,19 +1,46 @@
 import React, { useState } from "react";
-import { Input, Center, Button } from "native-base";
+import { Center, Button } from "native-base";
 import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { checkEnterCodeAction } from "../redux/slices/userReducer";
+import FormController from "../components/FormController";
 
 const EnterCodeScreen = (props) => {
+  const dispatch = useDispatch();
   const navigation = useNavigation();
+  const { email } = props?.route?.params;
   const [code, setCode] = useState("");
 
-  console.log(props?.route?.params?.email);
+  const userLoading = useSelector((state) => {
+    return state?.user?.loading;
+  });
+  const userError = useSelector((state) => {
+    return state?.user?.error;
+  });
+  const token = useSelector((state) => {
+    return state?.user?.token;
+  });
+
+  console.log(userLoading);
+  console.log(userError);
+  console.log(token);
 
   return (
     <Center flex={1}>
-      <Input value={code} onChangeText={setCode} placeholder="Code" w="300" />
+      <FormController
+        label="Enter Code"
+        message={"Code is incorrect!"}
+        errorMessageShow={userError?.status === 400}
+        value={code}
+        onChangeText={setCode}
+      />
       <Button
+        isDisabled={code === ""}
+        isLoading={userLoading}
         onPress={() => {
-          console.log("Enter Code");
+          dispatch(
+            checkEnterCodeAction({ email: email, enterCode: Number(code) })
+          );
         }}
         marginTop={5}
         size="sm"
@@ -23,7 +50,6 @@ const EnterCodeScreen = (props) => {
       <Button
         variant="link"
         onPress={() => {
-          console.log("Enter Code");
           navigation.navigate("Signin");
         }}
         size="sm"
