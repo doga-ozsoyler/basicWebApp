885971ff71426d48cb71796545b19436735e01bf
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-14 00:29:22 +0300

    Add get enter code controller for testing

--- api/routes/testRoutes.js
+++ api/routes/testRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const expressHandler = require("express-async-handler");
 const User = require("../models/user");
+const EnterCode = require("../models/enterCode");
 const makeToken = require("../helpers/makeToken");
 
 const testUserRoutes = express.Router();
@@ -12,6 +13,22 @@ testUserRoutes.post(
     const token = makeToken(user._id);
 
     res.status(201).json({ success: true, message: "Signin Success!", token });
+  })
+);
+
+testUserRoutes.get(
+  "/getEnterCode",
+  expressHandler(async (req, res) => {
+    const user = await User.findOne({ email: req?.body?.email });
+    const enterCode = await EnterCode.findById(user.enterCode);
+
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "Code is successfully returned!",
+        enterCode,
+      });
   })
 );
 
