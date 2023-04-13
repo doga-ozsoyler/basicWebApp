51a45d6ac691842c1e346147e8dec8945a8f97e7
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-04-13 17:10:23 +0300

    Add create user error tests

--- api/__test__/user.test.js
+++ api/__test__/user.test.js
@@ -1,12 +1,20 @@
 import app from "../server";
 import * as testDB from "../config/dbTestConnect";
 import supertest from "supertest";
+import makeToken from "../helpers/makeToken";
 
 const request = supertest(app);
 
-describe("first test", () => {
+describe("POST - createUserController", () => {
+  let testToken;
   beforeAll(async () => {
     await testDB.connect();
+
+    const testUserRes = await request.post("/api/testUser/makeToken").send({
+      email: "testUser@test.com",
+    });
+
+    testToken = testUserRes.body.token;
   });
 
   afterAll(async () => {
@@ -14,7 +22,25 @@ describe("first test", () => {
     await testDB.closeDatabase();
   });
 
-  test("first test", async () => {
-    expect(1).toEqual(1);
+  test("Should return error message, if authentication doesn't exist.", async () => {
+    const res = await request.post("/api/user/create").send({
+      email: "newEmail@test.com",
+    });
+
+    expect(res.body).toEqual({
+      success: false,
+      message: "Invalid Authentication",
+    });
+  });
+
+  test("Should return error message, if user already added.", async () => {
+    const res = await request
+      .post("/api/user/create")
+      .set("authorization", testToken)
+      .send({
+        email: "testUser@test.com",
+      });
+
+    expect(res.body).toEqual({ success: false, message: "User already exist" });
   });
 });
